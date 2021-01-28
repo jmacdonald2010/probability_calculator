7abb68f39450ffb752c789ace9784ba1e5c77ceb
James MacDonald III <jamesdmacdonaldiii@gmail.com>
2021-01-27 22:33:38 -0500

    01/27/2021 22:33 still working, going to bed, will continue tomorrow

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+01/27/2021 22:32
+Still working on finishing the code. Have not tested anything yet. Pushing to remote before going to bed.
+
 01/27/2021 09:46
 Projected started. Still building the experiment function. Nothing has been tested yet. Pushing to remote before going to work.
--- prob_calculator.py
+++ prob_calculator.py
@@ -23,7 +23,31 @@ class Hat:
             # this may need some addtl work, I'm not sure if the balls in the hat should reset each time the func is called, or if they are out of the hat until the hat is empty
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass # until i write the actual function
+    m = 0 # is # of successful experiments we run
+    # first thing is to run the experiment the # of times specified (which is to draw)
+    for i in range (0, num_experiments):
+        experiment_success = False
+        result = hat.draw(num_balls_drawn)
+        # then lets clean up the results into a new dict
+        org_result = {} # organized results, aka a dict
+        # go thru results of the hat.draw, compile those values into a dict
+        for ball in result:
+            if ball not in org_result:
+                org_result[ball] = 1
+            else:
+                org_result[ball] = org_result[ball] + 1
+        # compare results to expected_balls
+        # this needs some more fine-tuning to make sure it works right
+        for ball in expected_balls: # can't remember if this is how to loop thru keys, going to bed soon so I'll check later.
+            if expected_balls[ball] >= org_result[ball]:
+                continue
+            else:
+                experiment_success = False
+                break
+            experiment_success = True
+        if experiment_success == True:
+            m += 1
+        
 
 
 
